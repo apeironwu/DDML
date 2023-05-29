c307e9d5bc996b2a712aae2a60daccd8cb75a1b7
Apeiron WU <jeremary@icloud.com>
2023-05-29 14:01:36 +0800

    Simulation 1

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # DDML
 distributed double machine learning
+
+
+
+
+## Simulation 
+
+### Simulation 1
+
+partial linear regression model with known error density
+
+parameter setting: 
+- number of sites: $K = 5$
+- sample size of each site: $n = 100$,
+- dimensionality of covariates: $p_\beta$, 
+- error density: normal distribution with variance $\psi_u$ and $\psi_v$, 
+- 
+
+comments: 
+- no machine learning model, no overfitting, no data splitting.
+
+
+questions: 
+- correlation between $U$ and $V$ in data generation?
+- 
---
+++ code/simulation/sim1.py
@@ -0,0 +1,128 @@
+import numpy as np
+
+## random seed
+# np.random.seed(128)
+
+## parameter setting
+n = 50
+K = 3
+
+p_gamma = 3
+beta = 2
+
+mat_gamma = np.random.randn(K, p_gamma) * 4
+mat_mu = np.random.randn(K, p_gamma) * 4
+
+psi_u = 10
+psi_v = 4
+
+psi_u_inv = 1 / psi_u
+
+
+## data generation 
+
+#### correlated X
+covmat_X = np.fromfunction(lambda i, j: np.power(-0.7, np.abs(i - j)), (p_gamma, p_gamma))
+arr_X = np.random.multivariate_normal(np.zeros(p_gamma), covmat_X, K * n).reshape(K, n, p_gamma)
+
+# #### uncorrelated X
+# arr_X = np.random.randn(K, n, p_gamma)
+
+mat_U = np.random.randn(n, K) * np.sqrt(psi_u)
+mat_V = np.random.randn(n, K) * np.sqrt(psi_v)
+
+mat_D = np.zeros((n, K))
+mat_Y = np.zeros((n, K))
+
+for j in range(K): 
+    mat_D[:, j] = arr_X[j] @ mat_mu[j] + mat_V[:, j]
+    mat_Y[:, j] = mat_D[:, j] * beta + arr_X[j] @ mat_gamma[j] + mat_U[:, j]
+
+
+## initial estimation of beta and estimation of nuisance parameter
+vec_beta_est_local = np.zeros(K)
+
+mat_mu_est = np.zeros((K, p_gamma))
+mat_gamma_est = np.zeros((K, p_gamma))
+
+mat_D_X_cur = np.zeros((n, p_gamma + 1))
+vec_theta_est = np.zeros(p_gamma + 1)
+
+for j in range(K): 
+    mat_mu_est[j] = np.linalg.solve(arr_X[j].T @ arr_X[j], arr_X[j].T @ mat_D[:, j])
+
+    mat_D_X_cur = np.column_stack((mat_D[:, j], arr_X[j]))
+    vec_theta_est = np.linalg.solve(mat_D_X_cur.T @ mat_D_X_cur, mat_D_X_cur.T @ mat_Y[:, j])
+
+    vec_beta_est_local[j] = vec_theta_est[0]
+    mat_gamma_est[j] = vec_theta_est[1:]
+
+beta_est_ini = np.mean(vec_beta_est_local)
+
+print( (mat_gamma_est - mat_gamma) / np.abs(mat_gamma_est) )
+print( (mat_mu_est - mat_mu) / np.abs(mat_mu_est) )
+
+
+## operations in central site
+vec_beta_est_cen = np.zeros(K)
+
+## statistics from other sites
+vec_s_cur = np.zeros(n)
+vec_S_est = np.zeros(K)
+
+for j in range(K): 
+    vec_s_cur = (mat_D[:, j] - arr_X[j] @ mat_mu_est[j]) \
+        * (mat_Y[:, j] - mat_D[:, j] * beta_est_ini - arr_X[j] @ mat_gamma_est[j])
+    vec_S_est[j] = np.mean(vec_s_cur)
+
+S = np.mean(vec_S_est)
+
+for j_cen in range(K): 
+    vec_Y_cen = np.zeros(n)
+    vec_D_cen = np.zeros(n)
+    mat_X_cen = np.zeros((n, p_gamma))
+
+    vec_Y_res_cur = np.zeros(n)
+    vec_Y_res_cen = np.zeros(n)
+
+    mat_G_slope = np.zeros((n, K))
+
+    vec_Y_cen = mat_Y[:, j_cen]
+    vec_D_cen = mat_D[:, j_cen]
+    mat_X_cen = arr_X[j_cen]
+
+    for j in range(K): 
+        vec_Y_res_cur = vec_Y_cen - vec_D_cen * beta_est_ini - mat_X_cen @ mat_gamma_est[j]
+        vec_Y_res_cen = vec_Y_cen - vec_D_cen * beta_est_ini - mat_X_cen @ mat_gamma_est[j_cen]
+
+        mat_G_slope[:, j] = np.power(vec_Y_res_cur, 2) - np.power(vec_Y_res_cen, 2)
+        
+        mat_G_slope[:, j] = np.exp(-.5 * psi_u_inv * mat_G_slope[:, j])
+
+        print("density ratio: ", np.mean(mat_G_slope[:, j])) ## density ratio
+
+        mat_G_slope[:, j] *= (vec_D_cen - mat_X_cen @ mat_mu_est[j]) * vec_D_cen
+
+        print(" " * 40, "G slope: ", np.mean(mat_G_slope[:, j]))
+
+    G_slope = np.mean(mat_G_slope)
+
+
+    ## final estimation
+    beta_est_cen = beta_est_ini + S / G_slope
+
+    # print(eq_itcp, eq_slope)
+
+    vec_beta_est_cen[j_cen] = beta_est_cen
+
+beta_est = np.mean(vec_beta_est_cen)
+
+## output
+print("S estimation:       ", S)
+print("local estimation:   ", vec_beta_est_local)
+print("central estimation: ", vec_beta_est_cen)
+print("initial estimation: ", beta_est_ini)
+print("final estimation:   ", beta_est)
+
+print("=" * 40)
+print("bias", beta_est - beta_est_ini)
